806585b713f35d402d0038bcb699dd40b113a64d
pavel11988 <kossoua88@gmail.com>
2021-12-09 00:35:03 +0200

    task-1-completed + escape-close

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 
 
-
 const galleryContainer = document.querySelector('.gallery');
 const imagesMarkup = createImageCardsMarkup(galleryItems);
 const img = document.querySelector('.gallery__image');
@@ -10,6 +9,8 @@ const img = document.querySelector('.gallery__image');
 
 galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 galleryContainer.addEventListener('click', zoomImage);
+
+
 
 
 function createImageCardsMarkup (galleryItems) {
@@ -33,13 +34,21 @@ function createImageCardsMarkup (galleryItems) {
 
 
 function zoomImage (event) {
+    window.addEventListener('keydown', onEscPress);
     event.preventDefault();
     const isImage = event.target.classList.contains("gallery__image");
     if(!isImage)return;
     const instance = basicLightbox.create(`
         <img class="original-image" src='${event.target.getAttribute('data-source')}'>      
     `)
-    instance.show()
+    instance.show();
+
+    function onEscPress (event) {
+        if(event.code === 'Escape'){
+            instance.close();
+        }
+    }   
 }
+
 
 
