a5aa5b7bbad9c22b777d6dbba248cded50451c61
pavel11988 <kossoua88@gmail.com>
2021-12-09 23:01:40 +0200

    task 2 - completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryContainer.addEventListener('click', openSimpleLightBox)
+
+
+function createImageCardsMarkup (galleryItems) {
+
+    const markup = galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`}).join('');
+
+    return markup;
+}
+
+
+function openSimpleLightBox (event) {
+    event.preventDefault();
+    let gallery = new SimpleLightbox('.gallery a', {
+        captions:true,
+        captionDelay: 250,
+        captionSelector:'img',
+        captionType:'attr',
+        captionPosition:'bottom',
+        captionsData:'alt',
+    });
+
+    gallery.on('error.simplelightbox', function (e) {
+        console.log(e); // some usefull information
+    });
+}
